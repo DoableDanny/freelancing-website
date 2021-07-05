be38b5e2c0da11247421dec85e046c1aaccde2b3
Dan <dantheado@gmail.com>
2021-07-05 16:00:22 +0100

    Made hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,32 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
   return (
-    <div className="w-screen bg-gradient-to-r from-blue-700 to-blue-500 py-52">
-      <div className="container px-4 m-auto">
-        <h1 className="text-white font-serif text-6xl">
-          Get more clients to your garage
-        </h1>
-        <p className="text-white text-2xl mt-12">
-          Make it easy for people to find you online with a modern professional
-          website.
-        </p>
-        <button className="mt-12 bg-white rounded-full py-5 px-8 text-xl font-bold transform hover:scale-105 hover:underline transition ease-in-out duration-200">
-          Give us a call
-        </button>
+    <div className="bg-gradient-to-r from-blue-700 to-blue-500 py-40">
+      <div className="container px-4 mx-auto flex flex-wrap flex-col md:flex-row">
+        {/* Left col */}
+        <div className="flex flex-col items-center w-full text-center md:text-left md:items-start md:w-2/5">
+          <h1 className="text-white font-serif text-5xl lg:text-6xl">
+            Get more clients to your garage
+          </h1>
+          <p className="text-white text-xl lg:text-2xl mt-12">
+            Make it easy for people to find you online with a modern
+            professional website.
+          </p>
+          <button className="mt-12 bg-white shadow-lg rounded-full py-5 px-8 text-xl font-bold transform hover:scale-105 hover:underline transition ease-in-out duration-200 hover:shadow-inner focus:border-black border-2 text-gray-800">
+            Give us a call
+          </button>
+        </div>
+
+        {/* Right col */}
+        <div className="w-full md:w-3/5 py-6 text-center">
+          <StaticImage
+            className="w-4/5"
+            src="../images/hero.png"
+            alt="hero img"
+          />
+        </div>
       </div>
     </div>
   )
