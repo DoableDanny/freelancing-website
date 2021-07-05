1e774d7abd796f4d597b876e918f0d1c97c4e802
Dan <dantheado@gmail.com>
2021-07-05 16:13:28 +0100

    Rough footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-900 w-full py-4 text-white text-center">
+      Footer
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 import Hero from "../components/Hero"
 
 const IndexPage = () => {
@@ -7,6 +8,7 @@ const IndexPage = () => {
     <>
       <Header />
       <Hero />
+      <Footer />
     </>
   )
 }
