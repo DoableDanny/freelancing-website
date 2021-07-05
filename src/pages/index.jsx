b651010cc55661ebab223b2cf5d4dce8285fbb4f
Dan <dantheado@gmail.com>
2021-07-05 17:37:54 +0100

    WhatWeDo section made

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Hero from "../components/Hero"
+import Hero from "../components/Index/Hero"
+import WhatWeDo from "../components/Index/WhatWeDo"
 
 const IndexPage = () => {
   return (
     <Layout>
       <Hero />
+      <WhatWeDo />
     </Layout>
   )
 }
