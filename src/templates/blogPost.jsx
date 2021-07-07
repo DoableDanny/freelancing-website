c0c319903645a75b1ccd3ee00b00b4c0e545730a
Dan <dantheado@gmail.com>
2021-07-07 19:29:44 +0100

    Added tailwind typeography to style posts

--- src/templates/blogPost.jsx
+++ src/templates/blogPost.jsx
@@ -4,17 +4,20 @@ import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link } from "gatsby"
 
+import "../styles/blog-post.module.css"
+
 // Provide common components that can be used in mdx files without having to import.
 const shortcodes = { Link }
 
 export default function PageTemplate({ data: { mdx } }) {
   return (
-    <>
+    // The prose classes come from @tailwindcss/typography to style all of the HTML that comes from markdown.
+    <article className="prose prose-lg lg:prose-xl">
       <h1>{mdx.frontmatter.title}</h1>
       <MDXProvider components={shortcodes}>
         <MDXRenderer frontmatter={mdx.frontmatter}>{mdx.body}</MDXRenderer>
       </MDXProvider>
-    </>
+    </article>
   )
 }
 
