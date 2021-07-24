dd54eb949daed9842d8298c3f96f52d2043259ae
Dan <dantheado@gmail.com>
2021-07-24 17:56:26 +0100

    Added form handler

--- src/components/Index/Contact.tsx
+++ src/components/Index/Contact.tsx
@@ -1,7 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
+import { ChangeEventHandler } from "react"
 import Fade from "react-reveal/Fade"
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
 const Contact = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, phone, message }),
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error))
+
+    e.preventDefault()
+  }
+
   return (
     <section
       id="contact"
@@ -19,7 +43,6 @@ const Contact = () => {
           <form
             className="mx-auto"
             method="POST"
-            action="/success"
             data-netlify="true"
             name="contact"
             netlify-honeypot="bot-field"
@@ -32,18 +55,30 @@ const Contact = () => {
               label="Name"
               inputType="text"
               placeholder="Enter your name..."
+              value={name}
+              handleChange={(e: { target: HTMLInputElement }) =>
+                setName(e.target.value)
+              }
               required
             />
             <FormInput
               label="Email"
               inputType="email"
               placeholder="Enter your email..."
+              value={email}
+              handleChange={(e: { target: HTMLInputElement }) =>
+                setEmail(e.target.value)
+              }
               required
             />
             <FormInput
               label="Phone"
               inputType="tel"
               placeholder="Enter your phone number..."
+              value={phone}
+              handleChange={(e: { target: HTMLInputElement }) =>
+                setPhone(e.target.value)
+              }
             />
             <div>
               <label className="block mb-1">Message: *</label>
@@ -51,13 +86,21 @@ const Contact = () => {
                 className="w-full rounded h-40 text-black p-2 mb-3"
                 name="message"
                 placeholder="Your message here..."
+                onChange={e => setMessage(e.target.value)}
+                value={message}
                 required
               ></textarea>
             </div>
-
+            <p className="text-white">
+              {name}
+              {email}
+              {phone}
+              {message}
+            </p>
             <button
               type="submit"
               className="bg-green-500 rounded w-full py-3 hover:bg-green-400 transition duration-300 ease-in font-medium"
+              onSubmit={handleSubmit}
             >
               Send
             </button>
@@ -72,12 +115,16 @@ type FormInputProps = {
   label: string
   inputType: string
   placeholder: string
+  value: string
+  handleChange: any
   required?: boolean
 }
 const FormInput = ({
   label,
   inputType,
   placeholder,
+  value,
+  handleChange,
   required,
 }: FormInputProps) => {
   return (
@@ -91,6 +138,8 @@ const FormInput = ({
         type={inputType}
         className="rounded border-2 border-gray-500 p-2 text-black"
         placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
         required={required}
       ></input>
     </div>
